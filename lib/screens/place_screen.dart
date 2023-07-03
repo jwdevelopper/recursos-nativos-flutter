e88c1df35c02015f85dab710390246e5037b70d5
Jw Junior <jwdevelopper@gmail.com>
2023-07-03 16:37:46 -0300

    Inicio rep

---
+++ lib/screens/place_screen.dart
@@ -0,0 +1,55 @@
+import 'package:app_recursos_nativos/widgets/image_input.dart';
+import 'package:flutter/material.dart';
+
+class PlaceFormScreen extends StatefulWidget {
+  const PlaceFormScreen({super.key});
+
+  @override
+  State<PlaceFormScreen> createState() => _PlaceFormScreenState();
+}
+
+class _PlaceFormScreenState extends State<PlaceFormScreen> {
+  final _titleController = TextEditingController();
+
+  void _submitForm(){}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Novo Lugar"),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: _titleController,
+                      decoration: InputDecoration(
+                        labelText: 'Título',
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    ImageInput(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          ElevatedButton.icon(
+            onPressed: _submitForm,
+            icon: Icon(Icons.add),
+            label: Text('Adicionar'),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/places_list_screen.dart
@@ -0,0 +1,26 @@
+import 'package:app_recursos_nativos/utils/app_routes.dart';
+import 'package:flutter/material.dart';
+
+class PlacesListScreen extends StatelessWidget {
+  const PlacesListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Meus Lugares'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).pushNamed(AppRoutes.PLACE_FORM);
+            },
+            icon: Icon(Icons.add),
+          )
+        ],
+      ),
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
